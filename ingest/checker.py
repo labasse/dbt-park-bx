e7e365dd1599b0015172297ce28e11635f3b089c
Labasse <labasse@gmail.com>
2024-10-02 10:59:51 +0200

    Simple model

--- ingest/checker.py
+++ ingest/checker.py
@@ -2,3 +2,4 @@ import duckdb
 
 with duckdb.connect('../data/datalake.db') as db:
     db.sql("select * from st_park_p").show()
+    db.sql("select * from ta_type").show()
---
+++ transform/models/bronze/ta_type.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT 
+    ta_type AS type_name 
+FROM st_park_p
